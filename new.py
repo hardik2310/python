3e1ce95231777318708b803bb22230ab3513115c
gosai hardik <hardik.hello23@gmail.com>
2020-05-26 14:05:41 +0530

    Update new.py

--- new.py
+++ new.py
@@ -4,7 +4,7 @@ print("Python version")
 print (sys.version)
 print("Version info.")
 print (sys.version_info)
-with open(f'1.txt', 'w+') as f:
-    file.write('This is new file......')
+with open('1.txt', 'w+') as f:
+    f.write('This is new file......')
 print("Done")
 print('==========================================')
