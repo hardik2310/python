2f34c94bbdcc65458bc6833038ac879ad796a972
gosai hardik <hardik.hello23@gmail.com>
2020-05-26 14:04:23 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,10 @@
-print("hello world")
 import sys
+print('==========================================')
 print("Python version")
 print (sys.version)
 print("Version info.")
 print (sys.version_info)
+with open(f'1.txt', 'w+') as f:
+    file.write('This is new file......')
+print("Done")
+print('==========================================')
